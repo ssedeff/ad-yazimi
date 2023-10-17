ef557b41651b5316d76af3cdf650eb063a811f72
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-17 14:31:26 +0300

    Add files via upload

---
+++ ad/ad/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            string ad, soyad, yazdır;
+            ad = txtAd.Text;
+            soyad = txtSoyad.Text;
+            yazdır = ad + " " + soyad;
+            textBox1.Text = yazdır;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sil;
+            sil = " "; 
+            textBox1.Text = " ";
+        }
+    }
+}
